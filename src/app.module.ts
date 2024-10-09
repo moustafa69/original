50143ef3f6dee1133ca683acd182b500de725acc
moustafa shousha <moustafa.ahmed760@gmail.com>
2024-10-09 12:54:09 +0300

    create auth module folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { ShopModule } from './modules/shop/shop.module';
     SellerModule,
     AdminModule,
     UserModule,
+    ShopModule,
     EnvConfigModule.register(),
     ConfigModule.forRoot({
       isGlobal: true,
@@ -32,7 +33,6 @@ import { ShopModule } from './modules/shop/shop.module';
         };
       },
     }),
-    ShopModule,
   ],
   controllers: [],
   providers: [],
